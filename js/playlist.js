ec4b8ef789650dd560e57719d3f54c8ec1da3d54
Dnnacheta <dnnacheta@live.com>
2022-03-03 14:33:24 +0100

    JS script for playlist

--- js/playlist.js
+++ js/playlist.js
@@ -1,1 +1,39 @@
-
+let allMusic = [
+    {
+        name: "Leewards Sailing",
+        artist: "Lee Jimmy",
+        img: "album-1",
+        scr: "music-1"
+    },    
+    {
+        name: "Boaz",
+        artist: "Mickey S",
+        img: "album-2",
+        scr: "music-2"
+    },    
+    {
+        name: "Crazy, Here",
+        artist: "Ikkson Peterson",
+        img: "album-3",
+        scr: "music-3"
+    },    
+    {
+        name: "Home Sweet Home",
+        artist: "Jelly Beans",
+        img: "album-4",
+        scr: "music-4"
+    },    
+    {
+        name: "Fools Rush Gold Fools",
+        artist: "Smitherson",
+        img: "album-5",
+        scr: "music-1"
+    },      
+    {
+        name: "The Chemicals",
+        artist: "Willow Wonkas",
+        img: "album-5",
+        scr: "music-4"
+    },
+    
+];
